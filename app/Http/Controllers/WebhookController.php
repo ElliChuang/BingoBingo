6dfa559198357151645496ce040a6cb53e8d9277
ElliChuang <elli.chuang@leishan.com.tw>
2025-03-24 12:02:12 +0800

    feat:建立user follow事件

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,19 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Services\UserService;
+use App\Services\BingoCardService;
 use App\Services\LineBotService;
+use App\Services\UserRegisterService;
+use App\Services\UserStatusService;
 use Illuminate\Http\JsonResponse;
 
 class WebhookController extends Controller
 {
-    /**
-     * 使用者服務
-     *
-     * @var UserService
-     */
-    protected $userService;
-
     /**
      * lineBot 服務
      *
@@ -23,10 +18,34 @@ class WebhookController extends Controller
      */
     protected $lineBotService;
 
-    public function __construct(UserService $userService, LineBotService $lineBotService)
+    /**
+     * 使用者註冊服務
+     *
+     * @var UserRegisterService
+     */
+    protected $userRegisterService;
+
+    /**
+     * 使用者狀態管理服務
+     *
+     * @var UserStatusService
+     */
+    protected $userStatusService;
+
+    /**
+     * 賓果卡服務
+     *
+     * @var BingoCardService
+     */
+    protected $bingoCardService;
+
+
+    public function __construct(LineBotService $lineBotService, UserStatusService $userStatusService, UserRegisterService $userRegisterService, BingoCardService $bingoCardService)
     {
-        $this->userService = $userService;
         $this->lineBotService = $lineBotService;
+        $this->userStatusService = $userStatusService;
+        $this->userRegisterService = $userRegisterService;
+        $this->bingoCardService = $bingoCardService;
     }
 
     /**
@@ -37,16 +56,34 @@ class WebhookController extends Controller
      */
     public function handle(Request $request): JsonResponse
     {
-        $data = $request->all();
+        $events = $request->input('events', []);
 
-        foreach ($data['events'] as $event) {
-            if ($event['type'] == 'follow') {
-                $userId = $event['source']['userId'];
-                $replyText = $this->userService->registerUser($userId);
-                $this->lineBotService->replyMessage($event['replyToken'], $replyText);
+        foreach ($events as $event) {
+            $lineId = $event['source']['userId'];
+
+            switch ($event['type']) {
+                case 'follow':
+                    $this->handleFollow($event, $lineId);
+                    break;
+                case 'message':
+                    //$this->handleMessage($event, $lineId);
+                    break;
             }
         }
 
         return response()->json(['status' => 'ok']);
+    }
+
+    /**
+     * 處理 follow 事件
+     *
+     * @param array $event
+     * @param string $lineId
+     * @return void
+     */
+    private function handleFollow(array $event, string $lineId): void
+    {
+        $this->userRegisterService->registerUser($event, $lineId);
+        return;
     }
 }
--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -18,7 +18,7 @@ class LineBotService
     public function __construct()
     {
         // 只有在需要發送訊息時才初始化 LINE API 連線
-        $config = Configuration::getDefaultConfiguration()->setAccessToken(env('LINE_CHANNEL_ACCESS_TOKEN'));
+        $config = Configuration::getDefaultConfiguration()->setAccessToken(config('services.line.channel_access_token'));
         $client = new Client();
         $this->bot = new MessagingApiApi($client, $config);
     }
